0d6e5722567928bc64dd9f86c91c375a913e6319
jmunene <jmunene@ona.io>
2017-02-10 16:31:06 +0300

    JM:Added other sections on the farmer profile

--- data_wrangler.R
+++ data_wrangler.R
@@ -1,5 +1,89 @@
 library(ona)
+library(tidyr)
 #Read in the data
-Tdata <- onaDownload('Fruits','onahelp','jmunene','jmg@ona')
+Tdata <- onaDownload('Farmers_1_1','samwata','samwata','92samson19')
+Tdata <- as.data.frame(Tdata)
+# Recode Region variables
+levels(Tdata$location)[levels(Tdata$location)==1] <- 'Nairobi'
+levels(Tdata$location)[levels(Tdata$location)==2] <- 'Mombasa'
+levels(Tdata$location)[levels(Tdata$location)==3] <- 'Kisumu'
+#Seperate crops columns
+Tdata <- separate(data = Tdata, col = crops_owned, 
+                      into = c("Crop1", "Crop2","Crop3","Crop4","Crop5"), sep = " ")
+#Recode Crops
+Tdata$Crop1 <- replace(Tdata$Crop1, Tdata$Crop1=="1", "Maize")
+Tdata$Crop1 <- replace(Tdata$Crop1, Tdata$Crop1=="2", "Millet")
+Tdata$Crop1 <- replace(Tdata$Crop1, Tdata$Crop1=="3", "Wheat")
+Tdata$Crop1 <- replace(Tdata$Crop1, Tdata$Crop1=="4", "Beans")
+Tdata$Crop1 <- replace(Tdata$Crop1, Tdata$Crop1=="5", "Peas")
 
+Tdata$Crop2 <- replace(Tdata$Crop2, Tdata$Crop2=="1", "Maize")
+Tdata$Crop2 <- replace(Tdata$Crop2, Tdata$Crop2=="2", "Millet")
+Tdata$Crop2 <- replace(Tdata$Crop2, Tdata$Crop2=="3", "Wheat")
+Tdata$Crop2 <- replace(Tdata$Crop2, Tdata$Crop2=="4", "Beans")
+Tdata$Crop2 <- replace(Tdata$Crop2, Tdata$Crop2=="5", "Peas")
+
+Tdata$Crop3 <- replace(Tdata$Crop3, Tdata$Crop3=="1", "Maize")
+Tdata$Crop3 <- replace(Tdata$Crop3, Tdata$Crop3=="2", "Millet")
+Tdata$Crop3 <- replace(Tdata$Crop3, Tdata$Crop3=="3", "Wheat")
+Tdata$Crop3 <- replace(Tdata$Crop3, Tdata$Crop3=="4", "Beans")
+Tdata$Crop3 <- replace(Tdata$Crop3, Tdata$Crop3=="5", "Peas")
+
+Tdata$Crop4 <- replace(Tdata$Crop4, Tdata$Crop4=="1", "Maize")
+Tdata$Crop4 <- replace(Tdata$Crop4, Tdata$Crop4=="2", "Millet")
+Tdata$Crop4 <- replace(Tdata$Crop4, Tdata$Crop4=="3", "Wheat")
+Tdata$Crop4 <- replace(Tdata$Crop4, Tdata$Crop4=="4", "Beans")
+Tdata$Crop4 <- replace(Tdata$Crop4, Tdata$Crop4=="5", "Peas")
+
+
+Tdata$Crop5 <- replace(Tdata$Crop5, Tdata$Crop5=="1", "Maize")
+Tdata$Crop5 <- replace(Tdata$Crop5, Tdata$Crop5=="2", "Millet")
+Tdata$Crop5 <- replace(Tdata$Crop5, Tdata$Crop5=="3", "Wheat")
+Tdata$Crop5 <- replace(Tdata$Crop5, Tdata$Crop5=="4", "Beans")
+Tdata$Crop5 <- replace(Tdata$Crop5, Tdata$Crop5=="5", "Peas")
+
+#Merge crops
+Tdata$Crops <- paste(Tdata$Crop1,Tdata$Crop2,Tdata$Crop3,Tdata$Crop4,Tdata$Crop5,sep=",")
+Tdata <- as.data.frame(Tdata)
+
+#Seperate Training columns
+Tdata <- separate(data = Tdata, col = training_attended, 
+                  into = c("T1", "T2","T3","T4","T5"), sep = " ")
+#Recode Trainings
+Tdata$T1 <- replace(Tdata$T1, Tdata$T1=="1", "Agri-mix training program")
+Tdata$T1 <- replace(Tdata$T1, Tdata$T1=="2", "Cattle management program")
+Tdata$T1 <- replace(Tdata$T1, Tdata$T1=="3", "Dairy training program")
+Tdata$T1 <- replace(Tdata$T1, Tdata$T1=="4", "Custom harvesting program")
+Tdata$T1 <- replace(Tdata$T1, Tdata$T1=="5", "Fieldcrop training program")
+
+Tdata$T2 <- replace(Tdata$T2, Tdata$T2=="1", "Agri-mix training program")
+Tdata$T2 <- replace(Tdata$T2, Tdata$T2=="2", "Cattle management program")
+Tdata$T2 <- replace(Tdata$T2, Tdata$T2=="3", "Dairy training program")
+Tdata$T2 <- replace(Tdata$T2, Tdata$T2=="4", "Custom harvesting program")
+Tdata$T2 <- replace(Tdata$T2, Tdata$T2=="5", "Fieldcrop training program")
+
+Tdata$T3 <- replace(Tdata$T3, Tdata$T3=="1", "Agri-mix training program")
+Tdata$T3 <- replace(Tdata$T3, Tdata$T3=="2", "Cattle management program")
+Tdata$T3 <- replace(Tdata$T3, Tdata$T3=="3", "Dairy training program")
+Tdata$T3 <- replace(Tdata$T3, Tdata$T3=="4", "Custom harvesting program")
+Tdata$T3 <- replace(Tdata$T3, Tdata$T3=="5", "Fieldcrop training program")
+
+Tdata$T4 <- replace(Tdata$T4, Tdata$T4=="1", "Agri-mix training program")
+Tdata$T4 <- replace(Tdata$T4, Tdata$T4=="2", "Cattle management program")
+Tdata$T4 <- replace(Tdata$T4, Tdata$T4=="3", "Dairy training program")
+Tdata$T4 <- replace(Tdata$T4, Tdata$T4=="4", "Custom harvesting program")
+Tdata$T4 <- replace(Tdata$T4, Tdata$T4=="5", "Fieldcrop training program")
+
+Tdata$T5 <- replace(Tdata$T5, Tdata$T5=="1", "Agri-mix training program")
+Tdata$T5 <- replace(Tdata$T5, Tdata$T5=="2", "Cattle management program")
+Tdata$T5 <- replace(Tdata$T5, Tdata$T5=="3", "Dairy training program")
+Tdata$T5 <- replace(Tdata$T5, Tdata$T5=="4", "Custom harvesting program")
+Tdata$T5 <- replace(Tdata$T5, Tdata$T5=="5", "Fieldcrop training program")
+
+#Merge Trainings
+Tdata$Trainings <- paste(Tdata$T1,Tdata$T2,Tdata$T3,Tdata$T4,Tdata$T5,sep=",")
+
+#Save file
+setwd("D:\\Jmunene@Ona\\Dashboards\\Technoserve\\Techno_serve")
+write.csv(Tdata,file="Tdata.csv",row.names = FALSE)
 
--- profiles/app.R
+++ profiles/app.R
@@ -1,10 +1,25 @@
 library(shinydashboard)
 library(dplyr)
+library(xtable)
 
-data <- Tdata
+#Read the Data
+data <- read.csv("D:\\Jmunene@Ona\\Dashboards\\Technoserve\\Techno_serve\\Tdata.csv",header = TRUE)
+# Subset the Location details
+Ldata <- subset(data, select=c("location"))
+colnames(Ldata) <- "Farmer Location"
+# Subset the Crops details
+Cdata <- subset(data, select=c("Crops"))
+colnames(Cdata) <- "Crops grown by Farmer"
+# Subset the Training details
+Tdata <- subset(data, select=c("Trainings"))
+colnames(Tdata) <- "Trainings attended by Farmer"
+# Subset the Sales details
+Sdata <- subset(data, select=c("total_sales"))
+colnames(Sdata) <- "Total Sales of the Farmer"
 
+#The App
 ui <- dashboardPage(
-  dashboardHeader(title = "TNS-Monitoring Dashboard",titleWidth = 350),
+  dashboardHeader(title = "TNS-Monitoring Dashboard"),
   dashboardSidebar(disable=TRUE),
   dashboardBody(
     # Boxes need to be put in a row (or column)
@@ -14,27 +29,45 @@ ui <- dashboardPage(
         # The id lets us use input$tabset1 on the server to find the current tab
         id = "profiles", height = "750px",
         tabPanel("Farmers", 
-                 box(
-                   title = "Farmer ID",
+                 column(width =6,
+                   box(width = NULL,
+                   title = "Farmer ID",solidHeader = TRUE,
                    textInput(
                      inputId = 'farmer_id', 
                      label = 'Enter Farmer ID', 
                      value = "")
+                 )),
+                column(width = 6,
+                  box(width=NULL,
+                    "Farmer Photo",solidHeader = TRUE,
+                    htmlOutput("image"))),
+                fluidRow(
+                  box(width = 12, "Farmer Details",status = "primary",solidHeader = TRUE,
+                    tableOutput("Fdetails"))),
+                fluidRow(
+                  box(width = 12, "Crop Details",status = "primary",solidHeader = TRUE,
+                      tableOutput("Cdetails"))),
+                fluidRow(
+                  box(width = 12, "Training Details",status = "primary",solidHeader = TRUE,
+                      tableOutput("Tdetails"))),
+                fluidRow(
+                  box(width = 12, "Sales Details",status = "primary",solidHeader = TRUE,
+                      tableOutput("Sdetails")))
                  ),
-                 htmlOutput("image")
-                 ),
-        tabPanel("Agents", "Tab content 2")
+                tabPanel("Agents", "Tab content 2")
       ),
       tabBox(
-        title = "Sales",
+        title = "FBO Sales",
         # The id lets us use input$tabset1 on the server to find the current tab
         id = "sales", height = "750px",
-        tabPanel("table", "Sales Table"),
-        tabPanel("charts", "Sales charts")
+        tabPanel("Table", "FBO Sales"),
+        tabPanel("Dashboard", "Dashboard"),
+        tabPanel("Charts", "Charts")
       )
     )
+    )
   )
-)
+
 
 server <- function(input, output) {
   
@@ -43,9 +76,55 @@ server <- function(input, output) {
     select <- reactive({
       input$farmer_id})
     
-    data_f <- data %>% filter(data$X_uuid==select())
+    data_f <- data %>% filter(data$farmer_id==select())
 
-        tags$img(src = data_f$image)
+        tags$img(src = data_f$farmer_image,width=120,height=120)
+  })
+#Location Details Section
+  output$Fdetails <- renderTable({
+    
+    select <- reactive({
+      input$farmer_id})
+    
+    Sdata_f <- Ldata %>% filter(data$farmer_id==select())
+    
+ xtable(Sdata_f)
+
+  })
+# Crops Details Section
+  output$Cdetails <- renderTable({
+    
+    select <- reactive({
+      input$farmer_id})
+    
+    Sdata_f <- Cdata %>% filter(data$farmer_id==select())
+    
+    xtable(Sdata_f)
+    
+  })
+
+  # Trainings Details Section
+  output$Tdetails <- renderTable({
+    
+    select <- reactive({
+      input$farmer_id})
+    
+    Sdata_f <- Tdata %>% filter(data$farmer_id==select())
+    
+    xtable(Sdata_f)
+    
+  })
+
+# Sales Details Section
+  output$Sdetails <- renderTable({
+    
+    select <- reactive({
+      input$farmer_id})
+    
+    Sdata_f <- Sdata %>% filter(data$farmer_id==select())
+    
+    xtable(Sdata_f)
+    
   })
 }
 shinyApp(ui, server)
